5f449f79564e0ed10defb84733ce98536e9625e7
ameeraazli <ameera.azli@ytl.com>
2023-05-12 13:40:07 +0800

    Creating a Title Component

---
+++ components/Title.js
@@ -0,0 +1,19 @@
+import { Text, StyleSheet } from 'react-native';
+
+function Title({children}) {
+  return <Text style={styles.title}>Opponent's Guess</Text>
+}
+
+export default Title;
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#ddb52f',
+    textAlign: 'center',
+    borderWidth: 2,
+    borderColor: '#ddb52f',
+    padding: 12
+  }
+});
--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,9 +1,11 @@
 import { View, Text, StyleSheet } from 'react-native';
 
+import Title from '../components/Title';
+
 function GameScreen() {
   return(
   <View style={styles.screen}>
-    <Text>Opponent's Guess</Text>
+    <Title>Opponent's Guess</Title>
     {/* GUESS */}
     <View>
       <Text>Higher or lower?</Text>
@@ -20,5 +22,14 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     padding: 24
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#ddb52f',
+    textAlign: 'center',
+    borderWidth: 2,
+    borderColor: '#ddb52f',
+    padding: 12
   }
 });
