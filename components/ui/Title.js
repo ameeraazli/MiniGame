47fe07ecd5b5e2b5e9c47a6e7457b9135f62aec5
ameeraazli <ameera.azli@ytl.com>
2023-05-15 15:58:26 +0800

    Adding a (Foreground)/rounded image

--- components/ui/Title.js
+++ components/ui/Title.js
@@ -1,7 +1,7 @@
 import { Text, StyleSheet } from 'react-native';
 
 function Title({children}) {
-  return <Text style={styles.title}>Opponent's Guess</Text>
+  return <Text style={styles.title}>{children}</Text>
 }
 
 export default Title;
