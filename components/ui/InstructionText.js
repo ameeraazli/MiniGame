51ff3e3a667e86c947de2adacb883a03ab79f167
ameeraazli <ameera.azli@ytl.com>
2023-05-15 15:08:50 +0800

    Adding and using custom fonts with React Native apps

--- components/ui/InstructionText.js
+++ components/ui/InstructionText.js
@@ -9,7 +9,8 @@ export default InstructionText;
 
 const styles = StyleSheet.create({
   instructionText: {
+    fontFamily: 'open-sans',
     color: Colors.accent500,
-    fontSize: 24
+    fontSize: 24,
   }
 });
