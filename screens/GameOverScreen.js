478320128c72d124dda457caf18bd6707acaa49d
ameeraazli <ameera.azli@ytl.com>
2023-05-18 09:59:22 +0800

    further improvements - make the GameOver page scrollable so we can use the 'play again' button

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,4 +1,4 @@
-import { View, Image, StyleSheet, Text, useWindowDimensions } from 'react-native';
+import { View, Image, StyleSheet, Text, useWindowDimensions, ScrollView } from 'react-native';
 
 import Colors from '../constants/colors';
 import Title from '../components/ui/Title';
@@ -24,16 +24,18 @@ function GameOverScreen({roundsNumber, userNumber, onStartNewGame}) {
   };
 
   return (
-  <View style={styles.rootContainer}>
-    <Title>GAME OVER!</Title>
-    <View style={[styles.imageContainer, imageStyle]}>
-      <Image style={styles.image} source={require('../assets/images/success.png')} />
-    </View>
-    <Text style={styles.summaryText}>
-      Your phone needed <Text style={styles.highlight}>{roundsNumber}</Text> rounds to guess the number <Text style={styles.highlight}>{userNumber}</Text>
-    </Text>
-    <PrimaryButton onPress={onStartNewGame}>Start New Game</PrimaryButton>
-  </View>
+    <ScrollView style={styles.screen}>
+      <View style={styles.rootContainer}>
+        <Title>GAME OVER!</Title>
+        <View style={[styles.imageContainer, imageStyle]}>
+          <Image style={styles.image} source={require('../assets/images/success.png')} />
+        </View>
+        <Text style={styles.summaryText}>
+          Your phone needed <Text style={styles.highlight}>{roundsNumber}</Text> rounds to guess the number <Text style={styles.highlight}>{userNumber}</Text>
+        </Text>
+        <PrimaryButton onPress={onStartNewGame}>Start New Game</PrimaryButton>
+      </View>
+    </ScrollView>
   );
 }
 
@@ -42,6 +44,9 @@ export default GameOverScreen;
 // const deviceWidth = Dimensions.get('window').width;
 
 const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+  },
   rootContainer: {
     flex: 1,
     padding: 24,
