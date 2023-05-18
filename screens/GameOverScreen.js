beef9d0585e405bdf8e17896368cd135727ee28e
ameeraazli <ameera.azli@ytl.com>
2023-05-18 09:52:03 +0800

    further improvements - make imageSize dynamic based on mobile orientation

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,14 +1,32 @@
-import { View, Image, StyleSheet, Text, Dimensions } from 'react-native';
+import { View, Image, StyleSheet, Text, useWindowDimensions } from 'react-native';
 
 import Colors from '../constants/colors';
 import Title from '../components/ui/Title';
 import PrimaryButton from '../components/ui/PrimaryButton';
 
 function GameOverScreen({roundsNumber, userNumber, onStartNewGame}) {
+  const { width, height } = useWindowDimensions();
+
+  let imageSize = 300;
+
+  if (width < 380) {
+    imageSize = 150;
+  }
+
+  if (height < 400) {
+    imageSize = 80;
+  }
+
+  const imageStyle = {
+    width: imageSize,
+    height: imageSize,
+    borderRadius: imageSize / 2
+  };
+
   return (
   <View style={styles.rootContainer}>
     <Title>GAME OVER!</Title>
-    <View style={styles.imageContainer}>
+    <View style={[styles.imageContainer, imageStyle]}>
       <Image style={styles.image} source={require('../assets/images/success.png')} />
     </View>
     <Text style={styles.summaryText}>
@@ -21,7 +39,7 @@ function GameOverScreen({roundsNumber, userNumber, onStartNewGame}) {
 
 export default GameOverScreen;
 
-const deviceWidth = Dimensions.get('window').width;
+// const deviceWidth = Dimensions.get('window').width;
 
 const styles = StyleSheet.create({
   rootContainer: {
@@ -31,9 +49,9 @@ const styles = StyleSheet.create({
     alignItems: 'center'
   },
   imageContainer: {
-    width: deviceWidth < 380 ? 150 : 300,
-    height: deviceWidth < 380 ? 150 : 300,
-    borderRadius: deviceWidth < 380 ? 75 : 150,
+    // width: deviceWidth < 380 ? 150 : 300,
+    // height: deviceWidth < 380 ? 150 : 300,
+    // borderRadius: deviceWidth < 380 ? 75 : 150,
     borderWidth: 3,
     borderColor: Colors.primary800,
     overflow: 'hidden',
